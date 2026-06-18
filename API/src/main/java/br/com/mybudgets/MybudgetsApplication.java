d5d93e998a458e0e1135931ff7f3c01d4b6647a9
dnl-alm <danielfonseca.almeida@gmail.com>
2026-06-18 16:55:28 -0300

    iniciando projeto

---
+++ API/src/main/java/br/com/mybudgets/MybudgetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mybudgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybudgetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybudgetsApplication.class, args);
+	}
+
+}
